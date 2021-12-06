af44c5a5d27503b11bab8ef83aea1c67657d6b00
fania <hanifanianugroho@gmail.com>
2021-12-06 21:17:30 +0700

    first

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Prediksi Ukuran Baju</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="kotak">
+        <h2 class="judul">PREDIKSI UKURAN BAJU</h2>
+        <form method="post" action="index.php">
+            <input type="text" name="bb" class="bil" autocomplete="off" placeholder="Masukkan Berat Badan">
+            <input type="text" name="tb" class="bil" autocomplete="off" placeholder="Masukkan Tinggi Badan">
+            <input type="text" name="umur" class="bil" autocomplete="off" placeholder="Masukkan Umur">
+
+            <input type="text" placeholder="Hasil Prediksi" class="opt">
+            <input type="submit" name="prediksi" value="Prediksi" class="tombol">
+
+        </form>
+            <h4 class="judul">© 2021 Hanan Hanafi & Hani Fania N</h4>
+    </div>
+
+</body>
+
+</html>
